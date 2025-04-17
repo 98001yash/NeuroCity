19d772fd010f06004839f90cb9ad6db2618ec264
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-17 19:10:25 +0530

    open feign clients added

--- citizen-service/src/main/java/com/company/NeuroCity/citizen_service/CitizenServiceApplication.java
+++ citizen-service/src/main/java/com/company/NeuroCity/citizen_service/CitizenServiceApplication.java
@@ -2,8 +2,10 @@ package com.company.NeuroCity.citizen_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CitizenServiceApplication {
 
 	public static void main(String[] args) {
