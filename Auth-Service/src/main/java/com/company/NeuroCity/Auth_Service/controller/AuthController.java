67ad5999179a4f77bcf71d94036bc77857b0bb5c
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-17 17:12:40 +0530

    added the remaning controller of the Auth-service

--- Auth-Service/src/main/java/com/company/NeuroCity/Auth_Service/controller/AuthController.java
+++ Auth-Service/src/main/java/com/company/NeuroCity/Auth_Service/controller/AuthController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -36,4 +33,44 @@ public class AuthController{
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Signup failed: " + e.getMessage());
         }
     }
-}
+
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequestDto authRequestDto) {
+        try {
+            String token = authService.login(authRequestDto);
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            e.printStackTrace(); // Logs the full error
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Login failed: " + e.getMessage());
+        }
+    }
+
+
+        @GetMapping("/validate-token")
+        public Boolean validateToken(@RequestParam("token") String token) {
+            return jwtTokenProvider.validateToken(token);
+        }
+
+        @GetMapping("/user/{email}")
+        public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email) {
+            UserDto userDto = authService.getUserByEmail(email);
+            return ResponseEntity.ok(userDto);
+        }
+
+        @PutMapping("/user")
+        public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
+            UserDto updatedUser = authService.updateUser(userDto);
+            return ResponseEntity.ok(updatedUser);
+        }
+
+        @GetMapping("/user/id/{userId}")
+        public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+            log.info("Received request to fetch user by ID: {}", userId);
+            UserDto userDto = authService.getUserById(userId);
+            log.info("Returning user: {}", userDto);
+            return ResponseEntity.ok(userDto);
+        }
+
+    }
