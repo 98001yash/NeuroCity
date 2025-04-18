937032ccf8c6484a5ed51a7097a3b7eb64c54d6e
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-18 07:21:40 +0530

    added the exception class

---
+++ citizen-service/src/main/java/com/company/NeuroCity/citizen_service/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.company.NeuroCity.citizen_service.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
