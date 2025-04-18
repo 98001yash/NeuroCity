a64f6634a89b91e77f6d1fe137c4cd8f28c5035f
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-18 16:35:04 +0530

    added the getById controller

--- citizen-service/src/main/java/com/company/NeuroCity/citizen_service/controller/CitizenController.java
+++ citizen-service/src/main/java/com/company/NeuroCity/citizen_service/controller/CitizenController.java
@@ -5,10 +5,7 @@ import com.company.NeuroCity.citizen_service.dtos.CitizenDto;
 import com.company.NeuroCity.citizen_service.service.CitizenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/citizens")
@@ -20,5 +17,13 @@ public class CitizenController {
     @PostMapping
     public ResponseEntity<CitizenDto> register(@RequestBody CitizenDto dto){
         return ResponseEntity.ok(citizenService.createCitizen(dto));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CitizenDto> getById(@PathVariable Long id){
+        return citizenService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
