427a50b2dcf90a897d3dc2dab2632038268e9848
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-19 11:54:14 +0530

    added rhe exceptions, advices and modelMapper configuration in the alert-service

---
+++ alert-service/src/main/java/com/company/NeuroCity/alert_service/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.company.NeuroCity.alert_service.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
